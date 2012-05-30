135bec9f2d0bd3822ac8ec5af5f3330e66ba92e0
Bryan Hunter <bryan.hunter@fireflylogic.com>
2012-05-29 19:04:49 -0500

    Added notes

--- bryan_hunter+erlang+problem1/euler1.erl
+++ bryan_hunter+erlang+problem1/euler1.erl
@@ -2,12 +2,18 @@
 
 -export([solve/0]).
 
+
 solve() ->
     lists:sum(
       lists:filter(
 	fun(X) -> (X rem 5 == 0) or (X rem 3 == 0) end, 
 	lists:seq(1,1000))).
 
-% notes:
+
+% Notes:
+% If we list all the natural numbers below 10 that are multiples of 3
+% or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+% Find the sum of all the multiples of 3 or 5 below 1000.
+%
 %  http://www.erlang.org/doc/man/lists.html
 %  http://www.erlang.org/doc/reference_manual/expressions.html
