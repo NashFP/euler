0c4ec671a44785aab1c7c6b5498a040179a1b45a
Calvin Bottoms <calvin.bottoms@gmail.com>
2012-06-23 18:20:08 -0500

    Added #32 in Haskell.

---
+++ calvinb+haskell/euler32.hs
@@ -0,0 +1,19 @@
+import Data.List
+
+size = 9
+perms = permutations $ take size $ "123456789"
+range = [1..size - 2]
+
+sliceSizes = nub [(x,y,z) | x <- range, y <- range, z <- range, 
+    x <= y, y <= z, x + y + z == size]
+
+combos = [(p,x,y,z) | p <- perms, (x,y,z) <- sliceSizes]
+
+triple p x y = map read [take x p, take y $ drop x p, drop (x + y) p] :: [Integer]
+
+triples = [triple p x y | (p,x,y,z) <- combos, x < y]
+pans = [(x,y,z) | [x,y,z] <- triples, x * y == z]
+sums = nub [s | (_,_,s) <- pans]
+
+main = do
+    print $ sum sums
