9378ff30ab48f099a84b9e3583a71ce6a9e7e7e8
Karlkim Suwamongkol <karlkim@outlook.com>
2013-09-15 16:39:39 -0500

    first two Project Euler's problems solved

---
+++ kimsk+fsharp/2-EvenFiboNumbers.fsx
@@ -0,0 +1,8 @@
+﻿let rec fibo (array:int[]) max :int[] =
+    let len = array.Length
+
+    if len < 2 then fibo [|1;2;|] max
+    elif array.[len-1] + array.[len-2] < max then fibo (Array.append array [|array.[len-1] + array.[len-2]|]) max
+    else array 
+
+fibo [||] 4000000 |> Array.filter (fun x -> x%2 = 0) |> Array.sum
