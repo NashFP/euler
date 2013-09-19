060c5a77e9c276cc420e11115fd07600a6679416
Karlkim Suwamongkol <karlkim@outlook.com>
2013-09-18 22:08:36 -0500

    Sum square difference problem solved

---
+++ kimsk+fsharp/6-sum-square-difference.fsx
@@ -0,0 +1,10 @@
+﻿let square x = x * x
+
+let rec sumOfSquare x =
+    match x with
+    | 0 -> 0
+    | x -> square x + sumOfSquare (x - 1)
+
+let squareOfSum x = [1..x] |> Seq.sum |> square
+
+squareOfSum 100 - sumOfSquare 100
